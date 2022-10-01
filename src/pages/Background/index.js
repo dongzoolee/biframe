55c4e457d3886b25af68ccc79db34e5c91c2cf12
DongZoo Lee <dongzoo@kodebox.io>
2022-10-02 02:45:43 +0900

    Apply the logics to the background service_worker script

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,20 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+const changeIcon = (value) =>
+  chrome.action.setIcon({ path: `./lgtm-${value}.png` });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'changeIcon') {
+    chrome.storage.local.set({ on: message.value });
+    changeIcon(message.value);
+    sendResponse({ status: 'ok' });
+  }
+});
+
+chrome.action.onClicked.addListener(async () => {
+  const { on } = (await chrome.storage.local.get('on')) || { on: false };
+
+  changeIcon(!on);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleIframe', value: !on });
+  });
+  chrome.storage.local.set({ on: !on });
+});
